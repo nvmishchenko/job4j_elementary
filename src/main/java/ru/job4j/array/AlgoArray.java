2e902b1fcf4e4d11bb88f2aa4c44c6131eb8ec99
Nikolai Mishchenko <progressivid@mail.ru>
2023-09-01 10:46:53 +0300

    1. Чтение кода и базовые ошибки [#40205 #441344]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 public class AlgoArray {
+
     public static void main(String[] args) {
         int[] array = new int[] {5, 3, 2, 1, 4};
         int temp = array[0];
@@ -12,7 +13,6 @@ public class AlgoArray {
         temp = array[3];
         array[3] = array[4];
         array[4] = temp;
-
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
